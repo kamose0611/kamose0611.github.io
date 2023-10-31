62c18578d6482aa55327499e3560cab693b2dbf2
かもせー <131740906+kamose0611@users.noreply.github.com>
2023-10-31 14:07:09 +0900

    Add files via upload

--- tesseract-ocr.js
+++ tesseract-ocr.js
@@ -1,7 +1,7 @@
 const resultElement = document.getElementById('result');
 Tesseract.recognize(
     image,
-    'eng', // 使用する言語を指定
+    'jpn', // 使用する言語を指定
     {
         logger: info => {
             console.log(info);
